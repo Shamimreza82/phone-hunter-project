c8b741768831cbfde945b8ba08e943635fcad5d4
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-28 15:06:49 +0600

    update code

--- js/phoneCart.js
+++ js/phoneCart.js
@@ -1,31 +1,59 @@
-const phoneHandaler = async () => {
-    const res = await fetch ('https://openapi.programming-hero.com/api/phones?search=iphone')
+const phoneHandaler = async (searchText) => {
+    const res = await fetch (`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json(); 
     const phone = data.data;
     phoneDisplay (phone)
 }
-phoneHandaler();
+// phoneHandaler();
 
-const phoneDisplay = (phone) => {
+
+
+const phoneDisplay = (phones) => {
+    console.log (phones)
     const phoneContainer = document.getElementById('phone-container')
-    phone.forEach(phone => {
+    phoneContainer.textContent= " ";
+    phones = phones.slice(0,12)
+    phones.forEach(phone => {
         console.log (phone);
         const div = document.createElement ('div'); 
         div.innerHTML = ` 
-                <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes"/></figure>
+                <div class="card bg-base-100 shadow-xl">
+                    <figure><img src="${phone.image}" alt="Shoes"/></figure>
                     <div class="card-body">
-                      <h2 class="card-title">Shoes!</h2>
-                      <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                      <h2 class="card-title">${phone.phone_name}</h2>
+                      <h2>Brand Name: ${phone.brand}</h2>
+                      <p >There are many variations of passages of available, but the majority have suffered</p>
+                      <div class="card-actions justify-center">
+                        <button onclick="butonHandaar('${phone.slug}')" class="btn btn-primary">Show Details</button>
                       </div>
                     </div>
                 </div>
         `; 
         phoneContainer.appendChild (div);
     });
-    
+}
 
 
+
+const serchHandle = () => {
+    const inputFild = document.getElementById("input-text"); 
+    const inputVlaue = inputFild.value; 
+    phoneHandaler (inputVlaue)
+}
+
+
+const searchHandeler2 = () => {
+    const searchHandaler2w = document.getElementById('input-text2'); 
+    const searchHandaler2Value = searchHandaler2w.value; 
+    phoneHandaler (searchHandaler2Value);
+
+}
+
+
+
+const butonHandaar = async (id) => {
+    console.log (id)
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    console.log (data)
 }
